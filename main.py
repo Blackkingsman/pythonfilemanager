87ee24db8c9f1a9027563e97d850177956f54526
TJ the coder <tdphill0304@gmail.com>
2020-08-06 13:14:48 -0500

    This is working just fine and avoids overwriting duplicate named files

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import os
 import tkinter as tk
-from tkinter import filedialog,Text
+from tkinter import filedialog, Text
 from datetime import datetime
 import shutil
 
@@ -53,146 +53,186 @@ class Actions:
         global DIRECTORY_TO_WATCH
         self.directory = DIRECTORY_TO_WATCH.replace('/', '\\') + '\\'
 
+    def return_path(self, file_type, folder_name):
+        if len(file_type) != 0:
+            return file_type
+        else:
+            setpath = self.directory + '\\' + folder_name
+            if not os.path.exists(setpath):
+                os.makedirs(setpath)
+            return setpath
+
+    def get_next_filename(self, source, directory_path, destination, file_name):
+
+        num = 0
+        while os.path.exists(destination):
+            num += 1
+            # use rfind to find your file extension if there is one
+            period = file_name.rfind('.')
+            # this ensures that it will work with files without extensions
+            if period == -1:
+                period = len(file_name)
+
+            # create our new destination
+            # we could extract the number and increment it
+            # but this allows us to fill in the gaps if there are any
+            # it has the added benefit of avoiding errors
+            # in file names like this "test(sometext).pdf"
+            new_file = f'{file_name[:period]}({num}){file_name[period:]}'
+
+            destination = directory_path + '\\' + new_file
+        shutil.move(source + file_name,
+                    destination)
+
+    def fucking_moveit(self, bool_flag, sub_path, main_path, filename, file_ext):
+
+        file_name = f'{filename}{file_ext}'
+        destination = main_path + '\\' + file_name
+
+        #if true move file to its first directory
+        if bool_flag:
+            if os.path.isfile(destination):
+                self.get_next_filename(self.directory, main_path, destination, file_name)
+
+            else:
+                shutil.move(self.directory + f'{filename}{file_ext}',
+                        destination)
+        #if false then move file to sub directory
+        else:
+            destination = sub_path + "\\" + file_name
+            if os.path.isfile(destination):
+                self.get_next_filename(main_path + "\\" ,sub_path, destination, file_name)
+            else:
+                shutil.move(main_path + "\\" +file_name,
+                            destination)
+
+
+
+
+    def create_sub(self, diretory_path, f):
+        #creating timestamp based subdirectory
+        timecreated = time.strftime('%m/%d/%Y', time.gmtime(
+            os.path.getctime(diretory_path + '\\' + f'{f}')))
+        month, day, year = str(timecreated).split('/', 2)
+        daycreated = month + '.' + day + '.' + year
+
+        #checking to see if diretory already exists
+        # if it doesn't create new directory and  return the path
+        if not os.path.isdir(diretory_path + "\\"+ f'{daycreated}'):
+            subdirect = diretory_path + '\\' + f'{daycreated}'
+            os.makedirs(subdirect)
+            return subdirect
+        #if it does exist return path
+        else:
+           return diretory_path + "\\"+ f'{daycreated}'
     def clean_up(self):
-        global SVideos, sPDF,SExcel, SImages, SPrograms, SDocuments
-        videoformats = ['.mp4', '.mov', '.webm', '.mgg', '.mp2','.mpeg', '.mpe', '.mpv',
+        global SVideos, sPDF, SExcel, SImages, SPrograms, SDocuments
+        videoformats = ['.mp4', '.mov', '.webm', '.mgg', '.mp2', '.mpeg', '.mpe', '.mpv',
                         '.ogg', '.m4p', '.m4v', '.avi', '.wmv', '.mov', '.qt',
                         '.flv', '.swf', '.avchd']
-        excelformats = ['.xlsm', 'xls', 'xlsx', 'xlsm', 'xlsb', '.xltx']
+        excelformats = ['.xlsm', '.xls', '.xlsx', '.xlsm', '.xlsb', '.xltx']
         pdfformats = ['.pdf']
-        docformats = ['.doc','.docm','.docx', '.dot', '.dotm', '.dotx', '.rtf', '.txt'
-                      , '.wps', '.xml', '.xps']
-        imageformats = ['.jpeg', '.jpg', '.png', '.gif', '.webp','.tiff', '.psd', '.raw', '.bmp'
-            ,'.heif', '.indd', '.jpeg 2000', '.jpe', '.jif','.jfif', '.jfi']
+        docformats = ['.doc', '.docm', '.docx', '.dot', '.dotm', '.dotx', '.rtf', '.txt'
+            , '.wps', '.xml', '.xps']
+        imageformats = ['.jpeg', '.jpg', '.png', '.gif', '.webp', '.tiff', '.psd', '.raw', '.bmp'
+            , '.heif', '.indd', '.jpeg 2000', '.jpe', '.jif', '.jfif', '.jfi']
         progformats = ['.exe', '.lnk']
         audioformats = []
+        PowerPoints = []
         for f in os.listdir(self.directory):
             filename, file_ext = os.path.splitext(f)
-
             try:
 
                 if not file_ext:
                     pass
 
                 elif file_ext.lower() in (videoformats):
-                    # if directory is set by user store
-                    # in designated area otherwise save in current directory
-                    if len(SVideos) != 0:
-                        videopath = SVideos
 
-                    else:
-                        videopath = self.directory + '\\Videos'
-                        if not os.path.exists(videopath):
-                            os.makedirs(videopath)
-                    if len(sPDF) != 0:
-                        pdf_path = sPDF
+                    videopath = self.return_path(SVideos, "Videos")
+                    self.fucking_moveit(1,"", videopath, filename, file_ext)
 
-                    print(videopath)
-                    print(self.directory)
-                    new_file = ""
-                    num = 0
-                    original_destination = f'{filename}{file_ext}'
-                    # loop until we find a file that doesn't exist
-                    while os.path.exists(videopath + '\\' + original_destination):
-                        num += 1
-                        print("stuck here")
+                    #create sub directory
 
-                        # use rfind to find your file extension if there is one
-                        period = filename.rfind('.')
-                        # this ensures that it will work with files without extensions
-                        if period == -1:
-                            period = len(filename)
+                    sub_directory = self.create_sub(videopath, f)
 
-                        # create our new destination
-                        # we could extract the number and increment it
-                        # but this allows us to fill in the gaps if there are any
-                        # it has the added benefit of avoiding errors
-                        # in file names like this "test(sometext).pdf"
-                        new_file = f'{filename[:period]}({num}){file_ext}'
-                        print(os.path.exists(new_file))
-                        original_destination = new_file
-                    if len(new_file) == 0:
-                        shutil.move(self.directory + f'{filename}{file_ext}',
-                                videopath + '\\' + f'{filename}{file_ext}')
-                    else:
-                        print("OH FUCK")
-                        print(new_file)
-                        shutil.move(self.directory + f'{filename}{file_ext}',
-                                    videopath + '\\' + new_file)
+                    #move the file to subdirectory
 
-                    for f in os.listdir(videopath):
-                        fname, filext = os.path.splitext(f)
-                        if not filext:
-                            pass
-                        elif filext.lower() in videoformats:
-                            timecreated = time.strftime('%m/%d/%Y', time.gmtime(
-                                os.path.getctime(videopath + '\\' + f'{f}')))
-                            month, day, year = str(timecreated).split('/', 2)
-                            daycreated = month + '.' + day + '.' + year
+                    self.fucking_moveit(0,sub_directory,videopath, filename, file_ext)
 
-                            if not os.path.exists(videopath + '\\' + f'{daycreated}'):
-                                os.makedirs(videopath + '\\' + f'{daycreated}')
-                            new_file = ""
-                            num = 0
-                            original_destination = f'{daycreated}' + '\\' + f'{f}'
-                            # loop until we find a file that doesn't exist
-                            while os.path.exists(videopath + '\\' + original_destination):
-                                num += 1
-                                print("stuck here 2")
 
-                                # use rfind to find your file extension if there is one
-                                period = fname.rfind('.')
-                                # this ensures that it will work with files without extensions
-                                if period == -1:
-                                    period = len(fname)
 
-                                # create our new destination
-                                # we could extract the number and increment it
-                                # but this allows us to fill in the gaps if there are any
-                                # it has the added benefit of avoiding errors
-                                # in file names like this "test(sometext).pdf"
-                                new_file = f'{fname[:period]}({num}){filext}'
-                                if (os.path.exists(videopath + '\\' + f'{daycreated}' + '\\' + new_file)):
-                                    original_destination = videopath + '\\' + f'{daycreated}' + '\\' + new_file
-                                else:
-                                    original_destination = new_file
-                            if len(new_file) == 0:
-                                print("Overwrite")
-                                shutil.move(videopath + '\\' + f'{f}',
-                                            videopath + '\\' + f'{daycreated}' + '\\' + f'{f}')
-                            else:
-                                print("OH FUCK 2")
-                                print(new_file)
-                                shutil.move(videopath + '\\' + f'{f}',
-                                            videopath + '\\' + f'{daycreated}' + '\\' + new_file)
+                elif file_ext.lower() in (excelformats):
 
-                '''elif file_ext.lower() in ('.mp4'):
-                    if not os.path.exists(self.directory + 'Gaming Videos'):
-                        os.makedirs(self.directory + 'Gaming Videos')
-                    shutil.move(self.directory + f'{filename}{file_ext}',
-                                self.directory + 'Gaming Videos\\' + f'{filename}{file_ext}')
-                    for f in os.listdir(self.directory + 'Gaming Videos'):
-                        fname, filext = os.path.splitext(f)
-                        if not filext:
-                            pass
-                        elif filext.lower() in ('.mp4'):
-                            timecreated = time.strftime('%m/%d/%Y', time.gmtime(
-                                os.path.getctime(self.directory + 'Gaming Videos\\' + f'{f}')))
-                            month, day, year = str(timecreated).split('/', 2)
-                            daycreated = month + '.' + day + '.' + year
+                    excel_path = self.return_path(SExcel, "Excel")
+                    self.fucking_moveit(1,"",excel_path, filename, file_ext)
 
-                            if not os.path.exists(self.directory + 'Gaming Videos\\' + f'{daycreated}'):
-                                os.makedirs(self.directory + 'Gaming Videos\\' + f'{daycreated}')
-                            shutil.move(self.directory + 'Gaming Videos\\' + f'{f}',
-                                        self.directory + 'Gaming Videos\\' + f'{daycreated}' + '\\' + f'{f}')'''
+                    # create sub directory
+
+                    sub_directory = self.create_sub(excel_path, f)
+
+                    # move the file to subdirectory
+
+                    self.fucking_moveit(0, sub_directory, excel_path, filename, file_ext)
+
+
+                elif file_ext.lower() in (docformats):
+                    document_path = self.return_path(SDocuments, "Documents")
+                    self.fucking_moveit(1,"",document_path, filename, file_ext)
+
+                    # create sub directory
+
+                    sub_directory = self.create_sub(document_path, f)
+
+                    # move the file to subdirectory
+
+                    self.fucking_moveit(0, sub_directory, document_path, filename, file_ext)
+
+                elif file_ext.lower() in (imageformats):
+                    image_path = self.return_path(SImages, "Images")
+                    self.fucking_moveit(1,"",image_path, filename, file_ext)
+
+                    # create sub directory
+
+                    sub_directory = self.create_sub(image_path, f)
+
+                    # move the file to subdirectory
+
+                    self.fucking_moveit(0, sub_directory, image_path, filename, file_ext)
+
+                elif file_ext.lower() in (pdfformats):
+                    pdf_path = self.return_path(sPDF, "PDF")
+                    self.fucking_moveit(1,"",pdf_path, filename, file_ext)
+
+                    # create sub directory
+
+                    sub_directory = self.create_sub(pdf_path, f)
+
+                    # move the file to subdirectory
+
+                    self.fucking_moveit(0, sub_directory, pdf_path, filename, file_ext)
+
+                elif file_ext.lower() in (progformats):
+                    program_path = self.return_path(SPrograms, "Programs")
+                    self.fucking_moveit(1,"",program_path, filename, file_ext)
+
+                    # create sub directory
+
+                    sub_directory = self.create_sub(program_path, f)
+
+                    # move the file to subdirectory
+
+                    self.fucking_moveit(0, sub_directory, program_path, filename, file_ext)
 
             except(FileNotFoundError, PermissionError):
                 pass
+
+
 def start_watching():
     a = Actions()
     a.clean_up()
     w = Watcher()
     w.run()
+
 
 def create_directory_path():
     global DIRECTORY_TO_WATCH
@@ -202,15 +242,16 @@ def create_directory_path():
     f.write(foldername)
     f.close()
 
+
 def gui():
     global DIRECTORY_TO_WATCH
     root = tk.Tk()
-    canvas = tk.Canvas(root, height = 700, width = 700, bg="#263D42")
+    canvas = tk.Canvas(root, height=700, width=700, bg="#263D42")
     canvas.pack()
     frame = tk.Frame(root, bg="white")
-    frame.place(relwidth=0.8, relheight= 0.8, relx= 0.1, rely= 0.1)
-    openFile = tk.Button(root, text ="Open File", padx=10, pady=5, bg="white",
-                         command = create_directory_path)
+    frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
+    openFile = tk.Button(root, text="Open File", padx=10, pady=5, bg="white",
+                         command=create_directory_path)
     openFile.pack()
     root.mainloop()
 
@@ -224,7 +265,7 @@ SExcel = ""
 sPDF = ""
 
 if __name__ == '__main__':
-    if not os.path.isfile(r'C:\Users\tdphi\PycharmProjects\pythonProject2\watchdirectory.txt'):
+    if not os.path.isfile(os.getcwd() + '\\' + 'watchdirectory.txt'):
         gui()
     else:
         f = open("watchdirectory.txt", "r")
@@ -232,4 +273,3 @@ if __name__ == '__main__':
             contents = f.read()
             DIRECTORY_TO_WATCH = contents
         start_watching()
-
